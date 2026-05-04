e56e2aeb8172449ba92a8df4b9dba453c9f28035
abubakarafzal0101 <abubakarafzal0101@gmail.com>
2026-05-04 20:31:30 +0500

    basic setup of backend server created

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// connection database
+connectDB();
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+if (process.env.NODE_ENV === "development") {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+}
+
+export default app;
