b80df9c11907482e8f462ffc7aa411d43a8eb70d
abubakarafzal0101 <abubakarafzal0101@gmail.com>
2026-05-05 21:55:20 +0500

    user upate or password routes created

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,5 @@
 import UserModel from "../models/userModel.js";
-
+import bcrypt from "bcryptjs";
 export const getCurrentUser = async (req, res) => {
   try {
     const userId = req.userId;
@@ -16,6 +16,80 @@ export const getCurrentUser = async (req, res) => {
     });
   } catch (error) {
     console.log(error, "Error in getting current user");
+    return res.status(500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { name, email } = req.body;
+    if (!name || !email) {
+      return res.status(400).json({
+        success: false,
+        message: "Name and email are required",
+      });
+    }
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    user.name = name;
+    user.email = email;
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error, "Error in updating user");
+    return res.status(500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
+
+export const updatePassword = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { oldPassword, newPassword } = req.body;
+    if (!oldPassword || !newPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Old password and new password are required",
+      });
+    }
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Old password is incorrect",
+      });
+    }
+    user.password = await bcrypt.hash(newPassword, 12);
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "Password updated successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error, "Error in updating password");
     return res.status(500).json({
       success: false,
       message: error.message || "Internal Server Error",
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getCurrentUser } from "../controllers/userControllers.js";
+import {
+  getCurrentUser,
+  updatePassword,
+  updateUser,
+} from "../controllers/userControllers.js";
 import isAuth from "../middlewares/isAuth.js";
 const userRouter = express.Router();
 
 // protected routes
 userRouter.get("/me", isAuth, getCurrentUser);
+userRouter.put("/update-profile", isAuth, updateUser);
+userRouter.put("/update-password", isAuth, updatePassword);
 
 export default userRouter;
